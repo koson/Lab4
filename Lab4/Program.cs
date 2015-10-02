9fa81b8d8a1553ae778406bc477679979642c081
Koson Trachu <koson.trachu@gmail.com>
2015-10-02 14:09:38 +0700

    first commit

---
+++ Lab4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;   // ไม่ได้ใช้ประโยชน์ ใน Lab นี้
+using System.Linq;                  // ไม่ได้ใช้ประโยชน์ ใน Lab นี้
+using System.Text;                  // ไม่ได้ใช้ประโยชน์ ใน Lab นี้
+using System.Threading.Tasks;       // ไม่ได้ใช้ประโยชน์ ใน Lab นี้
+
+namespace Lab4 
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            // การเขียน Control.WriteLine มีวิธีลัดคือ
+            // พิมพ์ cw แล้วกดปุ่ม Tab 2 ครั้ง
+            Console.WriteLine("Hello World!");
+
+            // การรันโปรแกรมโดยให้หน้าจอ Console ยังคงค้างอยู่ ไม่หายไป
+            // ทำได้โดยการกด Ctrl+F5
+            // หรือเลือกเมนู Debug->Start without debugging
+        }
+    }
+}
